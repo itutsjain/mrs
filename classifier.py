ed51444decfae0f49d3ce411f811947c1d2a8c41
stuti jain <80120812+itutsjain@users.noreply.github.com>
2022-05-29 01:45:04 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -15,15 +15,15 @@ class KNearestNeighbours:
         self.category_assigned = None
 
     @staticmethod
-    def dis(p1,p2):
-        """"Method returns the euclidean distance between two points"""
+    def dist(p1, p2):
+        """Method returns the euclidean distance between two points"""
         return np.linalg.norm(np.array(p1) - np.array(p2))
 
     def fit(self):
-        """method that performs the knn classification"""
-        # create a list of (distance, index) tuples from the test point to each point 
+        """Method that performs the KNN classification"""
+        # Create a list of (distance, index) tuples from the test point to each point in the data
         self.distances.extend([(self.dist(self.test_point, point), i) for point, i in zip(self.data, [i for i in range(len(self.data))])])
-       # Sort the distances in ascending order
+        # Sort the distances in ascending order
         sorted_li = sorted(self.distances, key=itemgetter(0))
         # Fetch the indices of the k nearest point from the data
         self.indices.extend([index for (val, index) in sorted_li[:self.k]])
